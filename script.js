9aa40e1da97650f5d5401c6fb28945611281cc64
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-05-05 03:30:06 +0000

    added auto background music

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-let spellcard = document.getElementById('cards')
-if (onClick === "on" && spellcard === "story1"){
-    document.getElementById('story1').classList.remove('d-none')
-    document.getElementById('story1').classList.add('d-block')
-    }
+let audio = document.getElementById('themeSong');
+audio.volume = 0.01;

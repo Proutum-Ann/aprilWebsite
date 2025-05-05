e63b132f4ddc7d3619bd589d6109e7f57348bd0a
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-05-05 03:01:45 +0000

    added 1 character page.

---
+++ script.js
@@ -0,0 +1,5 @@
+let spellcard = document.getElementById('cards')
+if (onClick === "on" && spellcard === "story1"){
+    document.getElementById('story1').classList.remove('d-none')
+    document.getElementById('story1').classList.add('d-block')
+    }

13957264e8acf53ef8f0cac760ab9f7cc1a00d89
Proutum-Ann <154941331+Proutum-Ann@users.noreply.github.com>
2025-05-06 02:01:50 +0000

    started character list

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 let audio = document.getElementById('themeSong');
 audio.volume = 0.01;
+
+//Guessing Game
+const characters = [
+    'Reimu Hakurei',
+    'Reimu Hakurei (L10.1)',
+    'Reimu Hakurei (L80)',
+    'Reimu Hakurei (A6)',
+    'Reimu Hakurei (A7A)',
+    'Reimu Hakurei (A7B)',
+    'Reimu Hakurei (AA)',
+    'Reimu Hakurei (B3)',
+    'Reimu Hakurei (C3)',
+    'Reimu Hakurei (Cf1#)',
+    'Reimu Hakurei (F1)',
+    'Marisa Kirisame',
+    'Marisa Kirisame (L10.1)',
+    'Marisa Kirisame (L80)',
+    'Marisa Kirisame (A6)',
+    'Marisa Kirisame (A7A)',
+    'Marisa Kirisame (A7B)',
+    'Marisa Kirisame (AA)',
+    'Marisa Kirisame (B3)',
+    'Marisa Kirisame (C3)',
+    'Marisa Kirisame (Cz1)',
+    'Marisa Kirisame (Cz1#)',
+    'Marisa Kirisame (Z1)',
+]
